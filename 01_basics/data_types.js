06f22836803e33685e560ecb05306edd8e9d04b6
Udit Saini <uditsaini8529@gmail.com>
2025-05-04 18:46:56 +0000

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all js codes as newer version
+
+let name = "Udit"
+let age = 23
+// supported data types :- int, string, boolean, null, undefined, symbol
+
+console.log(typeof null);
+console.log(typeof undefined);
